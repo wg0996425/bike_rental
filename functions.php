8799c5ee9eeb6cd179d89b7a4b63ddb2d2e2b720
Viktor Gray <vg0996425@otc.edu>
2025-10-20 10:05:06 -0500

    changed spacing

--- functions.php
+++ functions.php
@@ -1,33 +1,44 @@
 <?php
 function sqlAllCustomers() {
-    return "SELECT * FROM customers ORDER BY last_name, first_name;";
+    return "
+        SELECT * FROM customers 
+        ORDER BY last_name, first_name;
+    ";
 }
 
 function sqlAvailableBikes() {
-    return "SELECT bike_id, model, type, hourly_rate FROM bikes WHERE available = 1;";
+    return "
+        SELECT bike_id, model, type, hourly_rate FROM bikes 
+        WHERE available = 1;
+    ";
 }
 
 function sqlBikeRentals() {
     return "
-        SELECT b.model, c.first_name, c.last_name, r.start_time, r.end_time
-        FROM rentals r
+        SELECT b.model, c.first_name, c.last_name, r.start_time, r.end_time FROM rentals r
         JOIN bikes b ON r.bike_id = b.bike_id
-        JOIN customers c ON r.customer_id = c.customer_id;
-    ";
+        JOIN customers c ON r.customer_id = c.customer_id;";
 }
 
 function sqlMorningRentals() {
-    return "SELECT * FROM rentals WHERE start_time < '12:00:00' ORDER BY start_time;";
+    return "
+        SELECT * FROM rentals 
+        WHERE start_time < '12:00:00' 
+        ORDER BY start_time;
+    ";
 }
 
 function sqlTop3Bikes() {
-    return "SELECT * FROM bikes ORDER BY hourly_rate DESC LIMIT 3;";
+    return "
+        SELECT * FROM bikes 
+        ORDER BY hourly_rate 
+        DESC LIMIT 3;
+    ";
 }
 
 function sqlOpenRentals() {
     return "
-        SELECT r.rental_id, b.model, c.first_name, c.last_name, r.start_time
-        FROM rentals r
+        SELECT r.rental_id, b.model, c.first_name, c.last_name, r.start_time FROM rentals r
         JOIN bikes b ON r.bike_id = b.bike_id
         JOIN customers c ON r.customer_id = c.customer_id
         WHERE r.end_time IS NULL;
@@ -36,8 +47,7 @@ function sqlOpenRentals() {
 
 function sqlAllJoins() {
     return "
-        SELECT r.rental_id, b.model, b.type, b.hourly_rate, c.first_name, c.last_name, r.start_time, r.end_time
-        FROM rentals r
+        SELECT r.rental_id, b.model, b.type, b.hourly_rate, c.first_name, c.last_name, r.start_time, r.end_time FROM rentals r
         JOIN bikes b ON r.bike_id = b.bike_id
         JOIN customers c ON r.customer_id = c.customer_id
         ORDER BY r.start_time;
@@ -45,10 +55,18 @@ function sqlAllJoins() {
 }
 
 function sqlUpdateEndTime($rentalId) {
-    return "UPDATE rentals SET end_time = NOW() WHERE rental_id = $rentalId;";
+    return "
+        UPDATE rentals 
+        SET end_time = NOW() 
+        WHERE rental_id = $rentalId;
+    ";
 }
 
 function sqlUpdateBikeStatus($bikeId) {
-    return "UPDATE bikes SET available = 0 WHERE bike_id = $bikeId;";
+    return "
+        UPDATE bikes 
+        SET available = 0 
+        WHERE bike_id = $bikeId;
+    ";
 }
 ?>
